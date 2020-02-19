f08928adad8b90dd7888ada9d3e75d82c2dcb16a
Caio Almeida <caiosba@gmail.com>
2020-02-19 00:53:23 -0300

    Ticket #6211: Fixing unit tests

--- test/unit/create_team_task.spec.js
+++ test/unit/create_team_task.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { IntlProvider } from 'react-intl';
 import { mountWithIntl } from './helpers/intl-test';
 import { expect } from 'chai';
-import CreateTeamTask from '../../src/app/components/team/CreateTeamTask';
+import CreateTeamTask from '../../src/app/components/team/CreateTeamTaskModern';
 
 describe('<CreateTeamTask />', () => {
   it('should render create task button', function() {
