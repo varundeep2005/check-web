744d57897204262ed0f5f89be94ec90bbdf712d5
Caio Almeida <caiosba@gmail.com>
2020-03-23 16:05:21 -0300

    Ticket #6211: Fixing conflict

--- src/app/components/media/MoveDialog.js
+++ src/app/components/media/MoveDialog.js
@@ -3,7 +3,7 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import DestinationProjects from './DestinationProjects';
+import DestinationProjects from './DestinationProjectsModern';
 
 class MoveDialog extends React.Component {
   handleDestinationProjectsLoaded() {
