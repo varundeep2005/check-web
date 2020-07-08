a023255d8f54710b4a9037b9ea0049ac9842e1fd
Adam Hooper <adam@adamhooper.com>
2020-07-08 16:40:45 -0400

    <TeamPublicHeader>: subsume <TeamHeaderComponent>

--- src/app/components/team/TeamPublicHeader.js
+++ src/app/components/team/TeamPublicHeader.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import Relay from 'react-relay/classic';
+import { makeStyles } from '@material-ui/core/styles';
 import PublicTeamRoute from '../../relay/PublicTeamRoute';
+import TeamAvatar from './TeamAvatar';
 import teamPublicFragment from '../../relay/teamPublicFragment';
-import TeamHeaderComponent from './TeamHeaderComponent';
+import { HeaderTitle, headerHeight } from '../../styles/js/shared';
 
-const TeamPublicHeaderContainer = Relay.createContainer(TeamHeaderComponent, {
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    height: headerHeight,
+  },
+  avatarWrapper: {
+    padding: `${theme.spacing(0, 1)}`,
+  },
+}));
+
+function TeamPublicHeaderComponent(props) {
+  const classes = useStyles();
+  const settingsPage = props.children.props.route.path === ':team/settings';
+  const hideTeamName = /(.*\/project\/[0-9]+)/.test(window.location.pathname) || settingsPage;
+  const { team } = props;
+
+  if (!team) {
+    return null;
+  }
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.avatarWrapper}>
+        <TeamAvatar src={team.avatar} />
+      </div>
+      {hideTeamName ? null : (
+        <HeaderTitle>{team.name}</HeaderTitle>
+      )}
+    </div>
+  );
+}
+
+const TeamPublicHeaderContainer = Relay.createContainer(TeamPublicHeaderComponent, {
   fragments: {
     team: () => teamPublicFragment,
   },
