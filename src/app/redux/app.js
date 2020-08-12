acc3fd943623d0b761e513ad853e98d33387cb66
Adam Hooper <adam@adamhooper.com>
2020-08-12 09:43:49 -0400

    redux.ActionTypes: nix NOTIFY_*

--- src/app/redux/app.js
+++ src/app/redux/app.js
@@ -1,12 +1,9 @@
-import { NOTIFY_SEND, NOTIFY_RECEIVE, SET_CONTEXT } from './ActionTypes';
+import { SET_CONTEXT } from './ActionTypes';
 
 export default function app(state_ = { session: null }, action) {
   const state = state_;
   if (action.type === SET_CONTEXT) {
     state.context = action;
-    return state;
-  }
-  if (action.type === NOTIFY_SEND || action.type === NOTIFY_RECEIVE) {
     return state;
   }
   return action;
